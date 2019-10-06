d031b91152161bae3622fa47fe168620bc80e5d3
googlyy <googlybeast1@gmail.com>
2019-10-06 11:27:50 +0100

    Hoverable cards. Added background

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -8,6 +8,18 @@ const Sprite = styled.img`
     display: none;
 `;
 
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover {
+        box-shadow 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    }
+    -moz-user-select: none;
+    -website-user-select: none;
+    user-select: none;
+    -o-user-select: none;
+`;
+
 export default class PokemonCard extends Component {
     state = {
         name: '',
@@ -19,7 +31,7 @@ export default class PokemonCard extends Component {
 
     componentDidMount () {
         const {name, url} = this.props;
-        const pokemonIndex = url.split("/")[url.split('/').length - 2];
+        const pokemonIndex = url.split('/')[6];
         const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
 
         this.setState({
@@ -33,7 +45,7 @@ export default class PokemonCard extends Component {
 
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card">
+                <Card className="card">
                     <h5 className="card-header">{this.state.pokemonIndex}</h5>
                     {this.state.imageLoading ? (
                         <img 
@@ -67,7 +79,7 @@ export default class PokemonCard extends Component {
                             .join(' ')}
                         </h6>
                     </div>
-                </div>  
+                </Card>  
             </div>
         );
     }
